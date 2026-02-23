856bb475df147bcd781762e71a942e7b056df5ae
MohammedAnwar2244 <mohameanwar123@gmail.com>
2026-02-23 02:40:25 +0200

    edit version

--- rtl/MainBand/MAPPER/Mapper.sv
+++ rtl/MainBand/MAPPER/Mapper.sv
@@ -40,7 +40,7 @@ module Mapper #(
     //============================================================
     reg [$clog2(CLOCK_CYCLES_4)-1:0] cycle_count;
     reg [WIDTH-1:0] lane_data [0:NUM_LANES-1];
-    reg [8*N_BYTES-1:0] data_shift_reg;
+   
 
     integer i;
 
@@ -50,16 +50,27 @@ module Mapper #(
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
             cycle_count    <= 0;
-            data_shift_reg <= 0;
-            for (i = 0; i < NUM_LANES; i = i + 1)
-                lane_data[i] <= 0;
+            // Clear all lanes
+            lane_data[0]   <= 0;
+            lane_data[1]   <= 0;
+            lane_data[2]   <= 0;
+            lane_data[3]   <= 0;
+            lane_data[4]   <= 0;
+            lane_data[5]   <= 0;
+            lane_data[6]   <= 0;
+            lane_data[7]   <= 0;
+            lane_data[8]   <= 0;
+            lane_data[9]   <= 0;
+            lane_data[10]  <= 0;
+            lane_data[11]  <= 0;
+            lane_data[12]  <= 0;
+            lane_data[13]  <= 0;
+            lane_data[14]  <= 0;
+            lane_data[15]  <= 0;
+
         end
         else if (mapper_en) begin
-            // Clear all lanes before assignment
-            for (i = 0; i < NUM_LANES; i = i + 1)
-                lane_data[i] <= 0;
-
-            case (i_width_deg_map)
+            case (i_width_deg_map) 
 
             //====================================================
             // 16 Lanes Active
@@ -67,40 +78,58 @@ module Mapper #(
             DEGRADE_LANES_0_TO_15: begin
                 if (cycle_count < CLOCK_CYCLES_16) begin
                     if (cycle_count == 0) begin
-                        for (i = 0; i < NUM_LANES; i = i + 1)begin
-                            lane_data[i] <= i_in_data[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= i_in_data >> (NUM_LANES*WIDTH);
-                    end
-                    else begin
-                        for (i = 0; i < NUM_LANES; i = i + 1)begin
-                            lane_data[i] <= data_shift_reg[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= data_shift_reg >> (NUM_LANES*WIDTH);
-                    end
-                    cycle_count <= cycle_count + 1;
+                       
+                            lane_data [0]  <= {i_in_data[7:0],    i_in_data[135:128],i_in_data[263:256], i_in_data[391:384]};
+                            lane_data [1]  <= {i_in_data[15:8],   i_in_data[143:136],i_in_data[271:264],i_in_data[399:392]};
+                            lane_data [2]  <= {i_in_data[23:16],  i_in_data[151:144], i_in_data[279:272], i_in_data[407:400]};
+                            lane_data [3]  <= {i_in_data[31:24],  i_in_data[159:152], i_in_data[287:280], i_in_data[415:408]};
+                            lane_data [4]  <= {i_in_data[39:32],  i_in_data[167:160], i_in_data[295:288], i_in_data[423:416]};
+                            lane_data [5]  <= {i_in_data[47:40],  i_in_data[175:168], i_in_data[303:296], i_in_data[431:424]};
+                            lane_data [6]  <= {i_in_data[55:48],  i_in_data[183:176], i_in_data[311:304], i_in_data[439:432]};
+                            lane_data [7]  <= {i_in_data[63:56],  i_in_data[191:184], i_in_data[319:312], i_in_data[447:440]};
+                            lane_data [8]  <= {i_in_data[71:64],  i_in_data[199:192], i_in_data[327:320], i_in_data[455:448]};
+                            lane_data [9]  <= {i_in_data[79:72],  i_in_data[207:200], i_in_data[335:328], i_in_data[463:456]};
+                            lane_data [10] <= {i_in_data[87:80],  i_in_data[215:208], i_in_data[343:336], i_in_data[471:464]};
+                            lane_data [11] <= {i_in_data[95:88],  i_in_data[223:216], i_in_data[351:344], i_in_data[479:472]};
+                            lane_data [12] <= {i_in_data[103:96], i_in_data[231:224], i_in_data[359:352], i_in_data[487:480]};
+                            lane_data [13] <= {i_in_data[111:104],i_in_data[239:232], i_in_data[367:360], i_in_data[495:488]};
+                            lane_data [14] <= {i_in_data[119:112],i_in_data[247:240], i_in_data[375:368], i_in_data[503:496]};
+                            lane_data [15] <= {i_in_data[127:120],i_in_data[255:248], i_in_data[383:376], i_in_data[511:504]};  
                 end
-                
-            end
-
+                cycle_count <= cycle_count + 1 ;
+                end 
+                end
+            
             //====================================================
             // Lanes 0 → 7
             //====================================================
             DEGRADE_LANES_0_TO_7: begin
                 if (cycle_count < CLOCK_CYCLES_8) begin
-                    if (cycle_count == 0) begin
-                        for (i = 0; i < 8; i = i + 1)begin
-                            lane_data[i] <= i_in_data[i*WIDTH +: WIDTH];
-                        end 
+                    case (cycle_count ) 
+                    0:begin
+                    lane_data[0]  <= {i_in_data[7:0],    i_in_data[71:64],   i_in_data[135:128],  i_in_data[199:192]};
+                    lane_data[1]  <= {i_in_data[15:8],   i_in_data[79:72],   i_in_data[143:136],  i_in_data[207:200]};
+                    lane_data[2]  <= {i_in_data[23:16],  i_in_data[87:80],   i_in_data[151:144],  i_in_data[215:208]};
+                    lane_data[3]  <= {i_in_data[31:24],  i_in_data[95:88],   i_in_data[159:152],  i_in_data[223:216]};
+                    lane_data[4]  <= {i_in_data[39:32],  i_in_data[103:96],  i_in_data[167:160],  i_in_data[231:224]};
+                    lane_data[5]  <= {i_in_data[47:40],  i_in_data[111:104], i_in_data[175:168],  i_in_data[239:232]};
+                    lane_data[6]  <= {i_in_data[55:48],  i_in_data[119:112], i_in_data[183:176],  i_in_data[247:240]};
+                    lane_data[7]  <= {i_in_data[63:56],  i_in_data[127:120], i_in_data[191:184],  i_in_data[255:248]};
+                                        
+                    end
 
-                        data_shift_reg <= i_in_data >> (8*WIDTH);
+
+                    1: begin
+                        lane_data[0]  <= {i_in_data[263:256], i_in_data[327:320], i_in_data[391:384], i_in_data[455:448]};
+                        lane_data[1]  <= {i_in_data[271:264], i_in_data[335:328], i_in_data[399:392], i_in_data[463:456]};
+                        lane_data[2]  <= {i_in_data[279:272], i_in_data[343:336], i_in_data[407:400], i_in_data[471:464]};
+                        lane_data[3]  <= {i_in_data[287:280], i_in_data[351:344], i_in_data[415:408], i_in_data[479:472]};
+                        lane_data[4]  <= {i_in_data[295:288], i_in_data[359:352], i_in_data[423:416], i_in_data[487:480]};
+                        lane_data[5]  <= {i_in_data[303:296], i_in_data[367:360], i_in_data[431:424], i_in_data[495:488]};
+                        lane_data[6]  <= {i_in_data[311:304], i_in_data[375:368], i_in_data[439:432], i_in_data[503:496]};
+                        lane_data[7]  <= {i_in_data[319:312], i_in_data[383:376], i_in_data[447:440], i_in_data[511:504]};
                     end
-                    else begin
-                        for (i = 0; i < 8; i = i + 1)begin
-                            lane_data[i] <= data_shift_reg[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= data_shift_reg >> (8*WIDTH);
-                    end
+                    endcase
                     cycle_count <= cycle_count + 1;
                 end
             
@@ -110,83 +139,150 @@ module Mapper #(
             // Lanes 8 → 15
             //====================================================
             DEGRADE_LANES_8_TO_15: begin
-                if (cycle_count < CLOCK_CYCLES_8) begin
-                    if (cycle_count == 0) begin
-                        for (i = 0; i < 8; i = i + 1)begin
-                            lane_data[i+8] <= i_in_data[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= i_in_data >> (8*WIDTH);
+                
+                    if (cycle_count <CLOCK_CYCLES_8 ) begin
+                    case(cycle_count)
+                     0: begin
+                    lane_data[8]  <= {i_in_data[7:0],    i_in_data[71:64],   i_in_data[135:128],  i_in_data[199:192]};
+                    lane_data[9]  <= {i_in_data[15:8],   i_in_data[79:72],   i_in_data[143:136],  i_in_data[207:200]};
+                    lane_data[10]  <= {i_in_data[23:16],  i_in_data[87:80],   i_in_data[151:144],  i_in_data[215:208]};
+                    lane_data[11]  <= {i_in_data[31:24],  i_in_data[95:88],   i_in_data[159:152],  i_in_data[223:216]};
+                    lane_data[12]  <= {i_in_data[39:32],  i_in_data[103:96],  i_in_data[167:160],  i_in_data[231:224]};
+                    lane_data[13]  <= {i_in_data[47:40],  i_in_data[111:104], i_in_data[175:168],  i_in_data[239:232]};
+                    lane_data[14]  <= {i_in_data[55:48],  i_in_data[119:112], i_in_data[183:176],  i_in_data[247:240]};
+                    lane_data[15]  <= {i_in_data[63:56],  i_in_data[127:120], i_in_data[191:184],  i_in_data[255:248]};
+                                        
                     end
-                    else begin
-                        for (i = 0; i < 8; i = i + 1)begin
-                            lane_data[i+8] <= data_shift_reg[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= data_shift_reg >> (8*WIDTH);
+
+
+                     1: begin
+                        lane_data[8]  <= {i_in_data[263:256], i_in_data[327:320], i_in_data[391:384], i_in_data[455:448]};
+                        lane_data[9]  <= {i_in_data[271:264], i_in_data[335:328], i_in_data[399:392], i_in_data[463:456]};
+                        lane_data[10]  <= {i_in_data[279:272], i_in_data[343:336], i_in_data[407:400], i_in_data[471:464]};
+                        lane_data[11]  <= {i_in_data[287:280], i_in_data[351:344], i_in_data[415:408], i_in_data[479:472]};
+                        lane_data[12]  <= {i_in_data[295:288], i_in_data[359:352], i_in_data[423:416], i_in_data[487:480]};
+                        lane_data[13]  <= {i_in_data[303:296], i_in_data[367:360], i_in_data[431:424], i_in_data[495:488]};
+                        lane_data[14]  <= {i_in_data[311:304], i_in_data[375:368], i_in_data[439:432], i_in_data[503:496]};
+                        lane_data[15]  <= {i_in_data[319:312], i_in_data[383:376], i_in_data[447:440], i_in_data[511:504]};
                     end
+                    endcase
                     cycle_count <= cycle_count + 1;
                 end
-               
+            
             end
 
             //====================================================
             // Lanes 0 → 3
             //====================================================
-            DEGRADE_LANES_0_TO_3: begin
-                if (cycle_count < CLOCK_CYCLES_4) begin
-                    if (cycle_count == 0) begin   // <-- FIXED
-                        for (i = 0; i < 4; i = i + 1)begin
-                            lane_data[i] <= i_in_data[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= i_in_data >> (4*WIDTH);
-                    end
-                    else begin
-                        for (i = 0; i < 4; i = i + 1)begin
-                            lane_data[i] <= data_shift_reg[i*WIDTH +: WIDTH];
-                        end
-                        data_shift_reg <= data_shift_reg >> (4*WIDTH);
-                    end
-                    cycle_count <= cycle_count + 1;
-                end
+           DEGRADE_LANES_0_TO_3: begin
+    if (cycle_count < CLOCK_CYCLES_4) begin  // 4 cycles
+        case(cycle_count)
+            0: begin
+                lane_data[0] <= {i_in_data[7:0],   i_in_data[39:32],  i_in_data[71:64],  i_in_data[103:96]};
+                lane_data[1] <= {i_in_data[15:8],  i_in_data[47:40],  i_in_data[79:72],  i_in_data[111:104]};
+                lane_data[2] <= {i_in_data[23:16], i_in_data[55:48],  i_in_data[87:80],  i_in_data[119:112]};
+                lane_data[3] <= {i_in_data[31:24], i_in_data[63:56],  i_in_data[95:88],  i_in_data[127:120]};
             end
+            1: begin
+                lane_data[0] <= {i_in_data[135:128], i_in_data[167:160], i_in_data[199:192], i_in_data[231:224]};
+                lane_data[1] <= {i_in_data[143:136], i_in_data[175:168], i_in_data[207:200], i_in_data[239:232]};
+                lane_data[2] <= {i_in_data[151:144], i_in_data[183:176], i_in_data[215:208], i_in_data[247:240]};
+                lane_data[3] <= {i_in_data[159:152], i_in_data[191:184], i_in_data[223:216], i_in_data[255:248]};
+            end
+            2: begin
+                lane_data[0] <= {i_in_data[263:256], i_in_data[295:288], i_in_data[327:320], i_in_data[359:352]};
+                lane_data[1] <= {i_in_data[271:264], i_in_data[303:296], i_in_data[335:328], i_in_data[367:360]};
+                lane_data[2] <= {i_in_data[279:272], i_in_data[311:304], i_in_data[343:336], i_in_data[375:368]};
+                lane_data[3] <= {i_in_data[287:280], i_in_data[319:312], i_in_data[351:344], i_in_data[383:376]};
+            end
+            3: begin
+                lane_data[0] <= {i_in_data[391:384], i_in_data[423:416], i_in_data[455:448], i_in_data[487:480]};
+                lane_data[1] <= {i_in_data[399:392], i_in_data[431:424], i_in_data[463:456], i_in_data[495:488]};
+                lane_data[2] <= {i_in_data[407:400], i_in_data[439:432], i_in_data[471:464], i_in_data[503:496]};
+                lane_data[3] <= {i_in_data[415:408], i_in_data[447:440], i_in_data[479:472], i_in_data[511:504]};
+            end
+        endcase
+        cycle_count <= cycle_count + 1;
+    end
+end
 
             //====================================================
             // Lanes 4 → 7
-            //====================================================
             DEGRADE_LANES_4_TO_7: begin
-                if (cycle_count < CLOCK_CYCLES_4) begin
-                    if (cycle_count == 0) begin   // <-- FIXED
-                        for (i = 0; i < 4; i = i + 1)begin
-                            lane_data[i+4] <= i_in_data[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= i_in_data >> (4*WIDTH);
-                    end
-                    else begin
-                        for (i = 0; i < 4; i = i + 1)begin
-                            lane_data[i+4] <= data_shift_reg[i*WIDTH +: WIDTH];
-                        end 
-                        data_shift_reg <= data_shift_reg >> (4*WIDTH);
-                    end
-                    cycle_count <= cycle_count + 1;
-                end
-               
+    if (cycle_count < CLOCK_CYCLES_4) begin  // 4 cycles
+        case(cycle_count)
+            0: begin
+                lane_data[4] <= {i_in_data[7:0],   i_in_data[39:32],  i_in_data[71:64],  i_in_data[103:96]};
+                lane_data[5] <= {i_in_data[15:8],  i_in_data[47:40],  i_in_data[79:72],  i_in_data[111:104]};
+                lane_data[6] <= {i_in_data[23:16], i_in_data[55:48],  i_in_data[87:80],  i_in_data[119:112]};
+                lane_data[7] <= {i_in_data[31:24], i_in_data[63:56],  i_in_data[95:88],  i_in_data[127:120]};
             end
+            1: begin
+                lane_data[4] <= {i_in_data[135:128], i_in_data[167:160], i_in_data[199:192], i_in_data[231:224]};
+                lane_data[5] <= {i_in_data[143:136], i_in_data[175:168], i_in_data[207:200], i_in_data[239:232]};
+                lane_data[6] <= {i_in_data[151:144], i_in_data[183:176], i_in_data[215:208], i_in_data[247:240]};
+                lane_data[7] <= {i_in_data[159:152], i_in_data[191:184], i_in_data[223:216], i_in_data[255:248]};
+            end
+            2: begin
+                lane_data[4] <= {i_in_data[263:256], i_in_data[295:288], i_in_data[327:320], i_in_data[359:352]};
+                lane_data[5] <= {i_in_data[271:264], i_in_data[303:296], i_in_data[335:328], i_in_data[367:360]};
+                lane_data[6] <= {i_in_data[279:272], i_in_data[311:304], i_in_data[343:336], i_in_data[375:368]};
+                lane_data[7] <= {i_in_data[287:280], i_in_data[319:312], i_in_data[351:344], i_in_data[383:376]};
+            end
+            3: begin
+                lane_data[4] <= {i_in_data[391:384], i_in_data[423:416], i_in_data[455:448], i_in_data[487:480]};
+                lane_data[5] <= {i_in_data[399:392], i_in_data[431:424], i_in_data[463:456], i_in_data[495:488]};
+                lane_data[6] <= {i_in_data[407:400], i_in_data[439:432], i_in_data[471:464], i_in_data[503:496]};
+                lane_data[7] <= {i_in_data[415:408], i_in_data[447:440], i_in_data[479:472], i_in_data[511:504]};
+            end
+        endcase
+        cycle_count <= cycle_count + 1;
+    end
+end
 
             default: begin
-                    for (i = 0; i < NUM_LANES; i = i + 1) begin
-                        lane_data[i] <= {WIDTH{1'b0}};
-                    end
-                    data_shift_reg <= 0;
-                end
+            cycle_count    <= 0;
+            // clear data 
+            lane_data[0]   <= 0;
+            lane_data[1]   <= 0;
+            lane_data[2]   <= 0;
+            lane_data[3]   <= 0;
+            lane_data[4]   <= 0;
+            lane_data[5]   <= 0;
+            lane_data[6]   <= 0;
+            lane_data[7]   <= 0;
+            lane_data[8]   <= 0;
+            lane_data[9]   <= 0;
+            lane_data[10]  <= 0;
+            lane_data[11]  <= 0;
+            lane_data[12]  <= 0;
+            lane_data[13]  <= 0;
+            lane_data[14]  <= 0;
+            lane_data[15]  <= 0;
+            end
             endcase
         end
         else begin
             // IDLE state
-            for (i = 0; i < NUM_LANES; i = i + 1) begin
-                lane_data[i] <= {WIDTH{1'b0}};
+            cycle_count     <= 0;
+            //clear data 
+            lane_data[0]    <= 0;
+            lane_data[1]    <= 0;
+            lane_data[2]    <= 0;
+            lane_data[3]    <= 0;
+            lane_data[4]    <= 0;
+            lane_data[5]    <= 0;
+            lane_data[6]    <= 0;
+            lane_data[7]    <= 0;
+            lane_data[8]    <= 0;
+            lane_data[9]    <= 0;
+            lane_data[10]   <= 0;
+            lane_data[11]   <= 0;
+            lane_data[12]   <= 0;
+            lane_data[13]   <= 0;
+            lane_data[14]   <= 0;
+            lane_data[15]   <= 0;
             end
-            cycle_count <= 0;
-            data_shift_reg <= 0;
-        end
     end
     
     //============================================================
