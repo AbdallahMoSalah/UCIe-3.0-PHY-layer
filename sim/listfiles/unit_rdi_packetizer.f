9c49310fb0f8a682c12f8088c5074b4deecc7eef
abdelrahmanadwe <abdelrahmanadwe@gmail.com>
2026-02-25 03:21:27 +0200

    update unit_rdi_packetizer listfile

--- sim/listfiles/unit_rdi_packetizer.f
+++ sim/listfiles/unit_rdi_packetizer.f
@@ -1,13 +1,13 @@
 # Include directories
-+incdir+../../rtl/common
-+incdir+../../rtl/SideBand/common
-+incdir+../../rtl/SideBand/LinkMgmt
++incdir+rtl/common
++incdir+rtl/SideBand/common
++incdir+rtl/SideBand/LinkMgmt
 
 # Packages (لو فيه)
-../../rtl/SideBand/common/sb_pkg.sv
+rtl/SideBand/common/sb_pkg.sv
 
 # DUT
-../../rtl/SideBand/LinkMgmt/RDI_Packetizer.sv
+rtl/SideBand/LinkMgmt/RDI_Packetizer.sv
 
 # Testbench
-../../tb/unit/sideband/rdi_packetizer/RDI_Packetizer_tb.sv
+tb/unit/sideband/rdi_packetizer/RDI_Packetizer_tb.sv
