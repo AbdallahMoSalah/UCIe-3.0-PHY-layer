cf1e501ecedbd2e203add6a80ddbb181a58e1cda
abdallah-salah <abdallahmohamedsalahelden@gmail.com>
2026-02-27 14:16:49 +0200

    add more enum for readability and easier coding

--- rtl/SideBand/common/sb_pkg.sv
+++ rtl/SideBand/common/sb_pkg.sv
@@ -1,81 +1,102 @@
 package sb_pkg;
 
-    typedef enum logic [4:0] {
-        SB_32_MEM_READ   = 5'b00000,
-        SB_32_MEM_WRITE  = 5'b00001,
-        SB_32_DMS_REG_READ   = 5'b00010,
-        SB_32_DMS_REG_WRITE  = 5'b00011,
-        SB_32_CFG_READ   = 5'b00100,
-        SB_32_CFG_WRITE  = 5'b00101,
-        SB_64_MEM_READ   = 5'b01000,
-        SB_64_MEM_WRITE  = 5'b01001,
-        SB_64_DMS_REG_READ   = 5'b01010,
-        SB_64_DMS_REG_WRITE  = 5'b01011,
-        SB_64_CFG_READ   = 5'b01100,
-        SB_64_CFG_WRITE  = 5'b01101,
-        SB_COMPLETION_WITHOUT_DATA     = 5'b10000,
-        SB_COMPLETION_WITH_32_DATA        = 5'b10001,
-        SB_MSG_WITHOUT_DATA        = 5'b10010,
-        SB_MNGT_PORT_MSG_WITHOUT_DATA = 5'b10111,
-        SB_MNGT_PORT_MSG_WITH_DATA    = 5'b11000,
-        SB_COMPLETION_WITH_64_DATA        = 5'b11001,
-        SB_MSG_WITH_64_DATA        = 5'b11011,
-        SB_PRIORITY_MSG1 = 5'b11110,
-        SB_PRIORITY_MSG2   = 5'b11111
-    } sb_opcode_e;  
+  typedef enum logic [4:0] {  //Opcode enum
+    SB_32_MEM_READ                = 5'b00000,
+    SB_32_MEM_WRITE               = 5'b00001,
+    SB_32_DMS_REG_READ            = 5'b00010,
+    SB_32_DMS_REG_WRITE           = 5'b00011,
+    SB_32_CFG_READ                = 5'b00100,
+    SB_32_CFG_WRITE               = 5'b00101,
+    SB_64_MEM_READ                = 5'b01000,
+    SB_64_MEM_WRITE               = 5'b01001,
+    SB_64_DMS_REG_READ            = 5'b01010,
+    SB_64_DMS_REG_WRITE           = 5'b01011,
+    SB_64_CFG_READ                = 5'b01100,
+    SB_64_CFG_WRITE               = 5'b01101,
+    SB_COMPLETION_WITHOUT_DATA    = 5'b10000,
+    SB_COMPLETION_WITH_32_DATA    = 5'b10001,
+    SB_MSG_WITHOUT_DATA           = 5'b10010,
+    SB_MNGT_PORT_MSG_WITHOUT_DATA = 5'b10111,
+    SB_MNGT_PORT_MSG_WITH_DATA    = 5'b11000,
+    SB_COMPLETION_WITH_64_DATA    = 5'b11001,
+    SB_MSG_WITH_64_DATA           = 5'b11011,
+    SB_PRIORITY_MSG1              = 5'b11110,
+    SB_PRIORITY_MSG2              = 5'b11111
+  } sb_opcode_e;
 
-    typedef enum logic [2:0] {
-        STACK0   = 3'b000,
-        ADAPTER = 3'b001,
-        PHY = 3'b010,
-        MNGT_PORT_src = 3'b011,
-        STACK1  = 3'b100  
-    } sb_srcid_e;   
+  typedef enum logic [2:0] {  //srcid enum
+    STACK0 = 3'b000,
+    ADAPTER = 3'b001,
+    PHY = 3'b010,
+    MNGT_PORT_src = 3'b011,
+    STACK1 = 3'b100
+  } sb_srcid_e;
 
-    typedef enum logic [2:0] {
-        LOCAL_ADAPTER = 3'b001,
-        LOCAL_PHY = 3'b010,
-        REMOTE_ADAPTER = 3'b101,
-        REMOTE_PHY = 3'b110,
-        REMOTE_REG_ACCESS = 3'b100,
-        MNGT_PORT_dst = 3'b111
-    } sb_dstid_e;   
+  typedef enum logic [2:0] {  //dstid enum
+    LOCAL_ADAPTER = 3'b001,
+    LOCAL_PHY = 3'b010,
+    REMOTE_ADAPTER = 3'b101,
+    REMOTE_PHY = 3'b110,
+    REMOTE_REG_ACCESS = 3'b100,
+    MNGT_PORT_dst = 3'b111
+  } sb_dstid_e;
 
-    typedef enum logic [3:0] {
-        ACTIVE_REQ = 4'b0000,
-        L1_REQ = 4'b0001,
-        L2_REQ = 4'b0010,
-        LINK_RESET_REQ = 4'b0011,
-        LINK_ERROR_REQ = 4'b0100,
-        RETRAIN_REQ = 4'b0101,
-        DISABLE_REQ = 4'b0110,
-        ACTIVE_RSP = 4'b0111,
-        PMNAK_RSP = 4'b1000,
-        L1_RSP = 4'b1001,
-        L2_RSP = 4'b1010,
-        LINK_RESET_RSP = 4'b1011,
-        LINK_ERROR_RSP = 4'b1100,
-        RETRAIN_RSP = 4'b1101,
-        DISABLE_RSP = 4'b1110,
-        NOP = 4'b1111
-    } sb_rdi_msg_no_e;
+  typedef enum logic [3:0] {  //RDI message number enum
+    ACTIVE_REQ = 4'b0000,
+    L1_REQ = 4'b0001,
+    L2_REQ = 4'b0010,
+    LINK_RESET_REQ = 4'b0011,
+    LINK_ERROR_REQ = 4'b0100,
+    RETRAIN_REQ = 4'b0101,
+    DISABLE_REQ = 4'b0110,
+    ACTIVE_RSP = 4'b0111,
+    PMNAK_RSP = 4'b1000,
+    L1_RSP = 4'b1001,
+    L2_RSP = 4'b1010,
+    LINK_RESET_RSP = 4'b1011,
+    LINK_ERROR_RSP = 4'b1100,
+    RETRAIN_RSP = 4'b1101,
+    DISABLE_RSP = 4'b1110,
+    NOP = 4'b1111
+  } sb_rdi_msg_no_e;
 
-typedef struct packed {
+  typedef struct packed {
 
-    logic        dp;            // [63]
-    logic        cp;            // [62]
-    logic [2:0]  rsvd2;         // [61:59]
-    sb_dstid_e   dstid;         // [58:56]
-    logic [15:0] MsgInfo;       // [55:40]
-    logic [7:0]  MsgSubcode;    // [39:32]
+    logic        dp;          // [63]
+    logic        cp;          // [62]
+    logic [2:0]  rsvd2;       // [61:59]
+    sb_dstid_e   dstid;       // [58:56]
+    logic [15:0] MsgInfo;     // [55:40]
+    logic [7:0]  MsgSubcode;  // [39:32]
 
-    sb_srcid_e   srcid;         // [31:29]
-    logic [6:0]  rsvd1;         // [28:22]
-    logic [7:0]  msgcode;       // [21:14]
-    logic [8:0]  rsvd0;         // [13:5]
-    sb_opcode_e  opcode;        // [4:0]
-    
-} sb_header_t;
+    sb_srcid_e  srcid;    // [31:29]
+    logic [6:0] rsvd1;    // [28:22]
+    msg_code_e  msgcode;  // [21:14]
+    logic [8:0] rsvd0;    // [13:5]
+    sb_opcode_e opcode;   // [4:0]
+
+  } sb_header_t;
+
+  typedef enum logic [7:0] {
+    SBINIT_outofrst_code = 8'h91,
+    SBINIT_REQ_code      = 8'h95,
+    SBINIT_RESP_code     = 8'h9A,
+    MBINIT_REQ_code      = 8'hA5,
+    MBINIT_RESP_code     = 8'hAA,
+    MBTRAIN_REQ_code     = 8'hB5,
+    MBTRAIN_RESP_code    = 8'hBA,
+    RECAL_REQ_code       = 8'hD5,
+    RECAL_RESP_code      = 8'hDA,
+    PHYRETRAIN_REQ_code  = 8'hC5,
+    PHYRETRAIN_RESP_code = 8'hCA,
+    TRAINERROR_REQ_code  = 8'hE5,
+    TRAINERROR_RESP_code = 8'hEA,
+    RDI_REQ_code         = 8'h01,
+    RDI_RESP_code        = 8'h02,
+    TEST_REQ_code        = 8'h85,
+    TEST_RESP_code       = 8'h8A
+  } msg_code_e;
+
 
 
 endpackage
