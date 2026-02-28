ea2560a67aac8280e4b6a607d0512975a50176df
abdelrahmanadwe <abdelrahmanadwe@gmail.com>
2026-02-28 02:09:44 +0200

    add report extension feature to windows powershell script

--- run_sim.ps1
+++ run_sim.ps1
@@ -2,18 +2,21 @@ param(
     [string]$CONFIG,
     [string]$TOP,
     [string]$MODE   = "run",
-    [string]$SEED   = "default"
+    [string]$SEED   = "default",
+    [string]$REPORT_EXT   = "txt"
 )
 
 $SimDo = "sim/scripts/run.do"
 
 if (-not $CONFIG) {
     Write-Host "ERROR: CONFIG is required."
+    Write-Host "Example: ./run_sim.ps1 -CONFIG unit_rdi_packetizer -TOP RDI_Packetizer_tb"
     exit
 }
 
 if (-not $TOP) {
     Write-Host "ERROR: TOP is required."
+    Write-Host "Example: ./run_sim.ps1 -CONFIG unit_rdi_packetizer -TOP RDI_Packetizer_tb"
     exit
 }
 
@@ -21,7 +24,7 @@ if ($MODE -eq "debug") {
     vsim -do "set CONFIG $CONFIG; set TOP $TOP; set MODE debug; set SEED $SEED; do $SimDo"
 }
 elseif ($MODE -eq "report") {
-    vsim -c -do "set CONFIG $CONFIG; set TOP $TOP; set MODE report; set SEED $SEED; do $SimDo"
+    vsim -c -do "set CONFIG $CONFIG; set TOP $TOP; set MODE report; set REPORT_EXT $REPORT_EXT; set SEED $SEED; do $SimDo"
 }
 elseif ($MODE -eq "ci") {
     vsim -c -do "set CONFIG $CONFIG; set TOP $TOP; set MODE ci; set SEED $SEED; do $SimDo"
