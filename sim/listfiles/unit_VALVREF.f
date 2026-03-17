3c07387593eef31caec9941834f61545d91b3d3a
Yousef Mohamed Abdelfattah <fma2022yma@gmail.com>
2026-03-17 23:55:32 +0200

    Added the testbench file (VALVREF_tb.sv) and its related files. It tests the module "VALVREF" in this path:  rtl/MainSM/LTSM/MBTRAIN/VALVREF.sv the Added files are:    sim/coverage_cfg/VALVREF_tb.do    sim/listfiles/unit_VALVREF.f    sim/waves/VALVREF_tb.do    tb/unit/MainSM/LTSM/MBTRAIN/VALVREF_tb.sv

---
+++ sim/listfiles/unit_VALVREF.f
@@ -0,0 +1,11 @@
+# DUT
+rtl/common/UCIe_pkg.sv
+rtl/common/ltsm_if.sv
+rtl/MainSM/LTSM/D2C_PT/RX_D2C_PT.sv
+rtl/MainSM/LTSM/D2C_PT/TX_D2C_PT.sv
+rtl/MainSM/LTSM/MBTRAIN/VALVREF.sv
+# Some logic files for the Testbench
+tb/unit/MainSM/LTSM/MBTRAIN/ltsm_tb_attachments.sv
+
+# Testbench
+tb/unit/MainSM/LTSM/MBTRAIN/VALVREF_tb.sv
