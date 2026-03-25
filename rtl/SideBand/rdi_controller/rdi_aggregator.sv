e1ad61d056dfbce42cacf828dac4a1213b70a021
abdelrahmanadwe <abdelrahmanadwe@gmail.com>
2026-03-25 06:52:21 +0200

    finished sb aggregator design

--- rtl/SideBand/rdi_controller/rdi_aggregator.sv
+++ rtl/SideBand/rdi_controller/rdi_aggregator.sv
@@ -1,3 +1,5 @@
+import sb_pkg::*;
+
 module rdi_aggregator
 (
     input  logic        clk,
@@ -6,9 +8,10 @@ module rdi_aggregator
     input  logic [31:0] lp_cfg,
     input  logic        lp_cfg_vld,
 
-    output logic [127:0] lp_msg,
+    output sb_packet_t lp_msg,
     output logic         lp_msg_vld
 );
+
 
 typedef enum logic [1:0] {
     IDLE,
@@ -19,14 +22,18 @@ typedef enum logic [1:0] {
 state_t state, next_state;
 
 logic [127:0] packet_reg;
-logic [1:0]   chunk_cnt;
-logic [1:0]   expected_chunks;
+logic [2:0]   chunk_cnt;
+logic [2:0]   expected_chunks;
 
+sb_opcode_e opcode;
+
+logic [2:0]   cycle_count;
 ////////////////////////////////////////
 // opcode decode
 ////////////////////////////////////////
+assign opcode = sb_opcode_e'(lp_cfg[4:0]);
 
-function automatic logic [1:0] get_chunks(input logic [4:0] opcode);
+always_comb begin
 
     case(opcode)
 
@@ -40,14 +47,14 @@ function automatic logic [1:0] get_chunks(input logic [4:0] opcode);
         SB_COMPLETION_WITHOUT_DATA,
         SB_MSG_WITHOUT_DATA,
         SB_MNGT_PORT_MSG_WITHOUT_DATA:
-            get_chunks = 2;
+            cycle_count = 2;
 
         // header + 32 data
         SB_32_MEM_WRITE,
         SB_32_DMS_REG_WRITE,
         SB_32_CFG_WRITE,
         SB_COMPLETION_WITH_32_DATA:
-            get_chunks = 3;
+            cycle_count = 3;
 
         // header + 64 data
         SB_64_MEM_WRITE,
@@ -55,14 +62,14 @@ function automatic logic [1:0] get_chunks(input logic [4:0] opcode);
         SB_64_CFG_WRITE,
         SB_COMPLETION_WITH_64_DATA,
         SB_MSG_WITH_64_DATA:
-            get_chunks = 4;
+            cycle_count = 4;
 
         default:
-            get_chunks = 2;
+            cycle_count = 2;
 
     endcase
 
-endfunction
+end
 
 
 ////////////////////////////////////////
@@ -96,7 +103,10 @@ always_comb begin
             next_state = OUTPUT;
 
         OUTPUT:
-            next_state = IDLE;
+            if(lp_cfg_vld)
+                next_state = COLLECT;
+            else 
+                next_state = IDLE;
 
     endcase
 
@@ -113,13 +123,10 @@ always_ff @(posedge clk or negedge rst_n) begin
         packet_reg      <= '0;
         chunk_cnt       <= 0;
         expected_chunks <= 0;
-        lp_msg_vld      <= 0;
-        lp_msg          <= 0;
+
     end
 
     else begin
-
-        lp_msg_vld <= 0;
 
         case(state)
 
@@ -135,7 +142,7 @@ always_ff @(posedge clk or negedge rst_n) begin
                 packet_reg[31:0] <= lp_cfg;
                 packet_reg[127:32] <= '0;
 
-                expected_chunks <= get_chunks(lp_cfg[4:0]);
+                expected_chunks <= cycle_count;
 
                 chunk_cnt <= 1;
 
@@ -166,13 +173,45 @@ always_ff @(posedge clk or negedge rst_n) begin
 
         begin
 
-            lp_msg     <= packet_reg;
-            lp_msg_vld <= 1;
+            chunk_cnt <= 0;
+
+            if(lp_cfg_vld) begin
+
+                packet_reg[31:0] <= lp_cfg;
+                packet_reg[127:32] <= '0;
+
+                expected_chunks <= cycle_count;
+
+                chunk_cnt <= 1;
+
+            end
 
         end
 
         endcase
     end
 end
+
+always_comb begin
+
+    if(!rst_n) begin
+        lp_msg_vld = 0;
+        lp_msg     = 0;
+    end
+    else begin
+        case(state)
+
+            OUTPUT:
+            begin
+                lp_msg_vld = 1;
+                lp_msg     = sb_packet_t'(packet_reg);
+            end
+            default : lp_msg_vld = 0;
+
+        endcase
+    end
+    
+end
+
 
 endmodule
