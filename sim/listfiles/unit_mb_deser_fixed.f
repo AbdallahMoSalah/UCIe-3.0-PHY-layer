af6f42a012d15247b257ff3f5a63b037f5f94d8b
mohamed elsayed <mohamed.esayd.ebrahim.com>
2026-06-04 17:41:41 +0300

    Add mb_deserializer design and testbenches

---
+++ sim/listfiles/unit_mb_deser_fixed.f
@@ -0,0 +1,5 @@
+# SPEC-FIXED data deserializer (unsued/ copy) + thorough TB.
+rtl/MainBand/unsued/mb_deserializer.sv
+
+# Testbench
+rtl/MainBand/unsued/mb_deserializer_tb.sv
---
+++ sim/listfiles/unit_mb_deser_prod.f
@@ -0,0 +1,5 @@
+# Production (BROKEN) data deserializer -- demonstrates the spec mismatch.
+rtl/MainBand/MB_DeSerializer/mb_deserializer.sv
+
+# Testbench
+rtl/MainBand/unsued/mb_deserializer_tb.sv
