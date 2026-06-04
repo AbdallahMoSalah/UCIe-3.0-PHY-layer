2ef30a0faec455327c8fd5d6a06a8a82d6c34896
mohamed elsayed <mohamed.esayd.ebrahim.com>
2026-06-04 17:41:41 +0300

    Add mb_path_stream testbench

---
+++ sim/listfiles/unit_mb_path_stream.f
@@ -0,0 +1,10 @@
+# Streaming / lane-degrade integration of the MainBand datapath.
+rtl/MainBand/MAPPER/Mapper.sv
+rtl/MainBand/unsued/LFSR_TX.sv
+rtl/MainBand/MB_Serializer/mb_serializer.sv
+rtl/MainBand/unsued/mb_deserializer.sv
+rtl/MainBand/unsued/LFSR_RX.sv
+rtl/MainBand/DEMAPPER/Demapper.sv
+
+# Testbench
+rtl/MainBand/unsued/mb_path_stream_tb.sv
